b834b18271dc77698b5c660ab2b96d7c09648b9e
sumyataung2004 <sumyataung1584@gmail.com>
2022-07-23 18:17:52 +0630

    1st commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Paragraph Generator - Responsive Page
+
+## Overview
+
+  <h3>
+    <a href="https://colorflipper-by-sumyat.netlify.app/">
+      Demo - URL link - https://colorflipper-by-sumyat.netlify.app/
+    </a>
+  </h3>
+
+<br/>
+<br/>
+
+![](demo/large-screen.png)
+<br/>
+<br/>
+![](demo/large-screen-copy.png)
+<br/>
+<br/>
+![](demo/small-screen.png)
+<br/>
+<br/>
+
+## Built With
+
+- **HTML** ,
+- **CSS** and
+- **JS**
+
+## Features
+
+- **Responsive and look great on all devices**
+- **Random meaningful paragraph generating**
+- **Copy with one click**
---
+++ script.js
@@ -0,0 +1,171 @@
+// paragraph array
+let p = [
+  ` Intelligence is one of the most coveted traits in people. We admire
+those who are smart, and we often equate intelligence with success. But
+what is intelligence, really? And can everyone be intelligent?
+Intelligence has been defined in many ways, but most experts agree that
+it involves the ability to learn from experience, solve problems, and
+use knowledge to adapt to new situations. Intelligence also includes the
+ability to think abstractly and creatively. Some people are born with
+more intellectual firepower than others, but anyone can improve their
+intelligence through learning and practice. The good news is that
+intelligence is not a fixed trait; it can be enhanced through effort and
+education. The brain is like a muscle - the more you use it, the
+stronger it gets! So if you want to become smarter, start by challenging
+yourself intellectually every day. Read books on different subjects,
+take classes or online courses , solve puzzles or brain teasers , and
+engage in thoughtful conversations with others . The more you stimulate
+your mind , the sharper it will become .`,
+
+  `Personality is the set of individual characteristics that distinguishes
+one person from another. It includes both the psychological characteristics,
+such as attitudes and values, and the physical characteristics, such as hair
+color and height. Personality can be affected by a person's environment, experiences
+genetics, and hormones.There are many different theories about personality. 
+The most popular theory is the five-factor model, which suggests that there 
+are five basic dimensions of personality: openness to experience, conscientiousness,
+extraversion/introversion (the degree to which a person enjoys social interaction), 
+agreeableness (the degree to which a person is cooperative and helpful), 
+and neuroticism (the degree to which a person experiences negative emotions).
+Personality has been found to be fairly stable over time; people generally remain 
+consistent in their personalities throughout their lives. However, there may be 
+some changes in personality as people age. For example, older adults tend to 
+become more agreeable and less neurotic than they were when they were younger.`,
+
+  `Nature is one of the most important things in our lives. It provides us with
+the air we breathe, the water we drink, and the food we eat. It also provides 
+us with a place to play and relax. Nature is a source of beauty that can be 
+znjoyed by everyone. There are many ways to enjoy nature. One way is to go for  
+a walk or hike in a park or forest. Another way is to visit a beach or lakefront
+area. You can also enjoy nature from your own backyard by planting trees and 
+flowers, or setting up birdfeeders and benches. No matter how you choose to enjoy
+nature, it's important to remember that it's not just for looking at 
+it's for interacting with as well! Get out there and explore!`,
+
+  `As a responsible individual, it is important to take care of oneself and others. 
+This includes being mindful of one's words and actions, as well as taking into 
+account the potential consequences. In order to make the most positive impact in 
+the world, it is necessary to act with responsibility both on a personal level and 
+beyond.Being responsible means being accountable for one's own choices and behavior. 
+It requires honesty, integrity, accountability, and self-awareness. It also means 
+being considerate of others by behaving respectfully and mindfully considering their 
+feelings and needs. Acting responsibly allows people to build trust both within 
+themselves and with others. There are many ways that individuals can be responsible 
+in their everyday lives: showing up on time for work or school; eating healthy foods; 
+recycling materials instead of throwing them away; turning off lights when leaving a 
+room; not talking negatively about others behind their backs; etc.. The list goes on! 
+Ultimately, living responsibly comes down to making conscious decisions that have 
+positive impacts both individually and collectively - because we're all interconnected! 
+When more people adopt this mindset globally, we will see significant change in our 
+societies for the betterment of us all`,
+
+  `Love yourself. Its one of the simplest and most difficult things to do. 
+Loving yourself requires accepting all of your quirks, imperfections, and mistakes. 
+You have to be kind to yourself when you dont meet your own expectations and be 
+forgiving when you stumble.Loving yourself is about valuing who you are 
+intrinsically  not what others think of you or what material possessions 
+you may have. When we love ourselves, we can appreciate our own unique qualities 
+and see the beauty in life, even on the darkest days. We are more likely to 
+treat ourselves with kindness and respect which in turn allows us to better 
+care for those around us too. So how do we go about learning how to love 
+ourselves? It starts with being mindful of our thoughts  both positive 
+and negative towards ourselves throughout the day (and yes, this includes 
+self-talk in the shower!). Pay attention to how often you put yourself down 
+or criticize your actions; instead try replacing these thoughts with more 
+supportive ones that focus on your strengths instead! Make a point also of 
+honoring your needs by taking time out for activities that make YOU happy; whether 
+its reading a book, going for a walk outdoors or spending time with loved ones). 
+The bottom line is that if YOU dont love YOURSELF then nobody else will either!`,
+
+  `Its okay to be sad. Sadness is a natural emotion that everyone experiences 
+at some point in their lives. There are many reasons why people might feel sadness,
+including loss, disappointment, or loneliness.Sadness can make us feel down 
+and unmotivated. However, its important to remember that the feeling will 
+eventually pass and we can get through it. Some ways to deal with sadness 
+include talking to someone about how you're feeling, listening to music, 
+writing in a journal, or spending time outdoors.If sadness begins to 
+interfere with our daily life or lasts for an extended period of time, 
+it might be helpful to seek professional help from a therapist or counselor 
+who can provide additional support and guidance during this difficult time`,
+
+  `Love can be a beautiful thing, but for some it can also be a source of 
+great pain. When someone is in love, they may feel like they are on top of
+the world. However, when that person is not returned the feelings or is hurt 
+by their loved one, they can quickly fall into a deep depression. Some people 
+become so obsessed with the person that they love that they start to lose touch 
+with reality. They may begin to stalk or harass their loved one, which often 
+leads to disastrous results. In some cases, people have even killed themselves 
+because their love was not reciprocated. While love can make people do crazy 
+things, it is ultimately an emotion that should be cherished and enjoyed. 
+When two people are truly in love with each other, there is nothing else 
+in the world that matters more than being together.`,
+
+  ` Drug addiction is a serious problem in the United States. 
+According to the National Institute on Drug Abuse, drug addiction is a “chronic, 
+relapsing brain disease that is characterized by compulsive drug seeking and use, 
+despite harmful consequences.”Addiction can affect anyone, regardless of age, 
+race or socioeconomic status. The National Survey on Drug Use and Health found 
+that an estimated 22.5 million Americans aged 12 or older were addicted to drugs 
+or alcohol in 2013. This number includes 11 million people who abused prescription 
+drugs and 6 million people who abused illicit drugs.There are many different types 
+of drugs that can lead to addiction, including prescription medications, street 
+drugs and alcohol. No one knows exactly why some people become addicted while 
+others do not. However, scientists believe that genetic factors may play a role 
+in determining who becomes addicted to drugs. Environmental factors such as stressors 
+and exposure to addictive substances may also contribute to drug addiction.Once 
+someone becomes addicted to a drug, it can be very difficult for them to break free 
+from its grip . The National Institute on Drug Abuse reports that “drugs change 
+the brains structure and function… These changes persist long after you stop using 
+the drug…. This explains why quitting cold turkey is so difficult your brain has 
+been rewired so that using cocaine or heroin again feels normal."`,
+
+  `A career is an important aspect of life. It can provide purpose and 
+meaning, and it can be a way to make a difference in the world. 
+There are many different careers to choose from, and each has its 
+own unique set of benefits and challenges. When choosing a career, 
+it's important to consider your interests, skills, and values. 
+You should also think about the kind of work you want to do and the 
+lifestyle you want to lead. And don't forget to factor in financial 
+considerations too; making a living is important! Whatever career you 
+choose, be sure to invest time in developing your skills and building your 
+professional network. These things will help you succeed no matter what 
+field you're in.`,
+];
+
+// grabing necessary btn
+
+const btn = document.getElementById("btn");
+const input = document.getElementById("num");
+const copy = document.getElementById("fa");
+
+// grabing result para
+const para = document.getElementById("lorem");
+
+// Generate btn event
+btn.addEventListener("click", function () {
+  let Val = parseInt(input.value);
+  let random = Math.floor(Math.random() * p.length);
+  // empty
+  if (isNaN(Val) || Val <= 0 || Val > 9) {
+    para.innerHTML = `<p class="A"> ${p[random]} </p>`;
+  } else {
+    let result = p.slice(0, Val);
+    result = result
+      .map(function (item) {
+        return `<p class="A"> ${item} </p>`;
+      })
+      .join("");
+    para.innerHTML = result;
+    // console.log(result);
+  }
+});
+
+// copy paragraph
+copy.addEventListener("click", function () {
+  let span = document.getElementById("span");
+  span.style.visibility = "visible";
+  navigator.clipboard.writeText(para.textContent);
+  setTimeout(function () {
+    span.style.visibility = "hidden";
+  }, 1000);
+});
